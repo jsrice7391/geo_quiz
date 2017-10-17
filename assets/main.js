0669cb28f966ef69fcb5cea3f23ee50f45b02aa2
Justin Rice <jsrice7391@gmail.com>
2017-10-17 11:02:08 -0400

    Answers roll out and show the correct answer

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,6 @@
 var correct_answers = [];
 
-var array_wrong = ["196", "Gerogia", "Atlanta", "Boston", "Borneo", "Washington", "Dallas"];
 
-function get_rand_answers(array_wrong, array_correct = correct_answers) {
-    var array_for_out = [];
-    for (var i = 0; i <= array_correct.length; i++) {
-        array_for_out.push([])
-    }
-    for (var i = 0; i < array_for_out.length; i++) {
-        array_for_out[i][0] = array_correct[i];
-    }
-
-    console.log(array_for_out)
-
-
-}
 
 
 
@@ -28,11 +14,22 @@ $(document).ready(function() {
 
         $.ajax("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple", {
             success: function(response) {
+                console.log(response);
                 $.each(response.results, function(index, question) {
-                    correct_answers.push(question.correct_answer);
-                    get_rand_answers();
-                    $(".questions").append("<h3>" + question.question + "</h3>");
-                    $(".questions").append("<p>" + question.correct_answer + "</p>");
+                    $(".questions").append("<h3>" + question.question + "</h3>");;
+                    var correct_answer = [question.correct_answer];
+                    all_answers = correct_answer.concat(question.incorrect_answers);
+                    console.log(all_answers);
+                    for (let answer in all_answers) {
+                        if (all_answers[answer] == question.correct_answer) {
+                            $(".questions").append("<p class ='correct'>" + all_answers[answer] + "</p>");
+
+                        } else {
+                            $(".questions").append("<p>" + all_answers[answer] + "</p>");
+                        }
+
+                    }
+
                 });
 
             }
