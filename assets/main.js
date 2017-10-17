c340b31fdea7f22d5ebd247a5af7e2fb49ce321e
Justin Rice <jsrice7391@gmail.com>
2017-10-17 12:46:09 -0400

    It shuffles

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,15 @@ var correct_answers = [];
 
 
 
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
 
 
 
@@ -17,7 +26,9 @@ $(document).ready(function() {
                     $(".questions").append("<h3>" + question.question + "</h3>");
                     var correct_answer = [question.correct_answer];
                     all_answers = correct_answer.concat(question.incorrect_answers);
-                    console.log(all_answers);
+                    console.log("These are the answers: " + all_answers);
+                    shuffleArray(all_answers);
+                    console.log("This is the shuffled array: " + all_answers);
                     for (let answer in all_answers) {
                         if (all_answers[answer] == question.correct_answer) {
                             $(".questions").append("<li class ='correct'>" + all_answers[answer] + "</li>");
@@ -34,7 +45,6 @@ $(document).ready(function() {
 
     $(".questions").on("click", "li", function() {
         $(this).toggleClass("selected");
-
     });
 
 
