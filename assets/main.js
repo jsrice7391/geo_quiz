58633ff838fb417bb8b621b081050ecdf1e36a64
Justin Rice <jsrice7391@gmail.com>
2017-10-17 11:31:30 -0400

    Allows user to select

--- assets/main.js
+++ assets/main.js
@@ -8,33 +8,34 @@ var correct_answers = [];
 
 $(document).ready(function() {
 
-    console.log("loaded");
-
     $("#quest_gen").on("click", function() {
 
         $.ajax("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple", {
             success: function(response) {
                 console.log(response);
                 $.each(response.results, function(index, question) {
-                    $(".questions").append("<h3>" + question.question + "</h3>");;
+                    $(".questions").append("<h3>" + question.question + "</h3>");
                     var correct_answer = [question.correct_answer];
                     all_answers = correct_answer.concat(question.incorrect_answers);
                     console.log(all_answers);
                     for (let answer in all_answers) {
                         if (all_answers[answer] == question.correct_answer) {
-                            $(".questions").append("<p class ='correct'>" + all_answers[answer] + "</p>");
+                            $(".questions").append("<li class ='correct'>" + all_answers[answer] + "</li>");
 
                         } else {
-                            $(".questions").append("<p>" + all_answers[answer] + "</p>");
+                            $(".questions").append("<li>" + all_answers[answer] + "</li>");
                         }
-
                     }
-
                 });
 
             }
         })
     })
+
+    $(".questions").on("click", "li", function() {
+        $(this).toggleClass("selected");
+
+    });
 
 
 
